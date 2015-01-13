7c7694e67584dbea9502eae09471b5ecbea079cd
Marián Hlaváč <hlavac95@gmail.com>
2015-01-13 22:55:03 +0100

    Basic data reading from Sirius API (for testing purposes)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     
     $(document).foundation();
@@ -19,14 +21,14 @@ $(document).ready(function () {
             $("section#timetable").addClass("fade-inactive"); 
             
             // Upravit pozici, pokud by se box otevřel přes pravý kraj viewportu
-            if ($(this).offset().left + 300 >= $(window).width()) {
+            /*if ($(this).offset().left + 300 >= $(window).width()) {
                 var left = $(this).offset().left - $("section#timetable > .row").offset().left;
                 console.log(left);
                 var difference = $(this).offset().left - $(window).width();
                 left = left + difference - 20;
                 console.log([left, difference ]);
                 $(this)[0].style = "left: "+ left + "px";
-            }
+            } wrong , přepsat... */
 
             // Pozice parallel-details boxu
             var parallelBoxPos = { 
@@ -43,7 +45,7 @@ $(document).ready(function () {
     
     /** Zavření všech paralelek po kliknutí jinam */
     
-    $(".row.tt-row").click(function (e) {
+    $(".row.tt-row, section#timetable, div.gap, div.info-line").click(function (e) {
         closeParallels(); 
     }).children().click(function(e) {
       return false;
@@ -53,7 +55,7 @@ $(document).ready(function () {
     
     /** Funkce pro zavření paralelek */
     function closeParallels() {
-        $(".parallel").removeClass("parallel-extended").each(function(num,el) { $(el)[0].style = " "; });
+        /*$(".parallel").removeClass("parallel-extended").each(function(num,el) { $(el)[0].style = " "; });*/
         $(".parallel-details").slideUp(400);
         $("section#timetable").removeClass("fade-inactive"); 
     }
@@ -83,6 +85,96 @@ $(document).ready(function () {
     setTimeout(function() {
         $(".info-line .spinner").fadeOut(500);
     },2000);
+    
+    
+    /** Dočasné přihl. údaje na sirius */
+    $("input#accesstoken").val( $.cookie('sirius-accesstoken') );
+    $("input#username").val( $.cookie('sirius-username') );
+    
+    $("#savein").click(function() {
+        $.cookie('sirius-accesstoken', $("input#accesstoken").val(), { expires: 14 } );
+        $.cookie('sirius-username', $("input#username").val(), { expires: 14 } );
+        alert("Settings saved. Sup, "+$("input#username").val()+" :)");
+        window.location.reload();
+    });
+    
+    
+    
+    /** Chování rozvržení timetablu */
+    
+    $.get( "https://sirius.fit.cvut.cz/api/v1/people/" + $.cookie('sirius-username') + "/events?access_token=" + $.cookie('sirius-accesstoken') + "&from=2015-01-12T00:00:00&to=2015-01-19T23:59:59&limit=128" )
+        .done( function(d) { 
+            
+            if ( d.meta.count <= d.meta.limit )
+            {
+                showEvents( d.events );
+            }
+            else console.log("Too many data. App is not ready yet for making more requests. Sry :/");
+        
+        });
+    
+    
+    function showEvents( events )
+    {
+        var timeExtremes = findMinMaxTimes( events );
+        $( "#timetable" ).attr( "data-mintime", timeExtremes[0] ).attr( "data-maxtime", timeExtremes[1] );
+        
+        $.each( events, function ( num, el ) {
+            createEventElement( el );        
+        } );
+        
+        console.log( events );
+    }
+    
+    function getSecondsToday( momentDate )
+    {
+        return momentDate.hours() * 3600 + momentDate.minutes() * 60 + momentDate.seconds();
+    }
+    
+    function findMinMaxTimes( siriusEvents )
+    {
+        var min = getSecondsToday( moment( siriusEvents[0].starts_at ) );
+        var max = getSecondsToday( moment( siriusEvents[0].ends_at ) );        
+        
+        $.each( siriusEvents, function ( num, el ) {
+            var estart = getSecondsToday( moment( el.starts_at ) );
+            var eend = getSecondsToday( moment( el.ends_at ) );
+            
+            if (estart < min) min = estart;
+            if (eend > max) max = eend;
+        } );
+        
+        return [min, max];
+    }
+    
+    function createEventElement( siriusEventobj )
+    {
+        var element = $("<div>").addClass("parallel").attr("role","listitem")
+                                .attr("data-from", siriusEventobj.starts_at).attr("data-to", siriusEventobj.ends_at);
+        
+        element.append( $("<div>").addClass("inner-item") );
+        
+        element.children().append( $("<h1>").text( siriusEventobj.links.course ) );
+        element.children().append( $("<div>").addClass("place").text( siriusEventobj.links.room ) );
+        element.children().append( $("<div>").addClass("time")
+                                  .text( moment( siriusEventobj.starts_at ).format("h:mm") +"-"+ moment( siriusEventobj.ends_at ).format("h:mm") ) 
+                                 );
+        
+        $("#timetable .row.day").eq( parseInt( moment( siriusEventobj.starts_at ).format("d") ) -1 ).find(".day-parallels").append( element );
+        
+        setEventRightSize( $("#timetable"), element );
+    }
+    
+    function setEventRightSize( timetableDOMobj, eventDOMobj )
+    {
+        var tablestarts = $( timetableDOMobj ).attr( "data-mintime" ),
+            fulldiff = $( timetableDOMobj ).attr( "data-maxtime" ) - $( timetableDOMobj ).attr( "data-mintime" ),
+            eventstarts = moment( $( eventDOMobj ).attr("data-from") ), eventends = moment( $( eventDOMobj ).attr("data-to") ),
+            eventlength = eventends.diff(eventstarts, "seconds"),
+            leftoffset = getSecondsToday( eventstarts ) - tablestarts;
+        
+        $( eventDOMobj ).css("width", ((eventlength/fulldiff)*100)+"%").css("left", ((leftoffset/fulldiff)*100)+"%");
+    }
     
     
     
