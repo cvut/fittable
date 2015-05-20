8f17359e7bbd5667d286fb825696b2e6b8a4c7be
Marián Hlaváč <hlavac95@gmail.com>
2015-05-21 01:03:28 +0200

    Settings dropdown

---
+++ src/js/components/FunctionSettings.component.js
@@ -0,0 +1,85 @@
+/**
+ * React component
+ * @author Marián
+ */
+
+export default class FunctionSettings extends React.Component
+{
+    constructor( props )
+    {
+        super.constructor( props );
+    }
+
+    toggle()
+    {
+        if ( ! this.refs.rootEl.getDOMNode().classList.contains( 'hide' ) )
+            this.refs.rootEl.getDOMNode().classList.add( 'hide' );
+        else
+            this.refs.rootEl.getDOMNode().classList.remove( 'hide' );
+    }
+
+    /**
+     * Renders the component
+     */
+    render()
+    {
+        return <div className="function-settings hide" ref="rootEl">
+            <h2>rozložení</h2>
+            <div className="row">
+                <div className="column large-6">
+                    <a href="#" className="layout-selector active">
+                        <i className="fa fa-th-list"></i>
+                        řádkové
+                    </a>
+                </div>
+                <div className="column large-6">
+                    <a href="#" className="layout-selector">
+                        <i className="fa fa-th"></i>
+                        sloupcové
+                    </a>
+                </div>
+            </div>
+            <h2>nastavení</h2>
+            <div className="row">
+                <div className="column large-3">
+                    <div className="switch small">
+                        <input id="exampleRadioSwitch1" type="radio" checked name="testGroup" />
+                        <label for="exampleRadioSwitch1"></label>
+                    </div>
+                </div>
+                <div className="column large-9 switch-label">
+                    Rozlišovat typy barevně
+                </div>
+            </div>
+            <div className="row">
+                <div className="column large-3">
+                    <div className="switch small">
+                        <input id="exampleRadioSwitch1" type="radio" checked name="testGroup" />
+                        <label for="exampleRadioSwitch1"></label>
+                    </div>
+                </div>
+                <div className="column large-9 switch-label">
+                    Lorem ipsum
+                </div>
+            </div>
+            <div className="row">
+                <div className="column large-3">
+                    <div className="switch small">
+                        <input id="exampleRadioSwitch1" type="radio" checked name="testGroup" />
+                        <label for="exampleRadioSwitch1"></label>
+                    </div>
+                </div>
+                <div className="column large-9 switch-label">
+                    Dolor sit
+                </div>
+            </div>
+            <h2>about</h2>
+            <p>
+                Fittable is Species volares, tanquam fortis solem. Resistentia congregabos, tanquam bi-color nomen. Sunt fermiumes promissio dexter, rusticus diatriaes.<br />
+                <a href="#">Frequently asked questions</a><br />
+                <a href="#">Contact us</a>
+            </p>
+
+        </div>;
+    }
+}
--- src/js/components/FunctionsBar.component.js
+++ src/js/components/FunctionsBar.component.js
@@ -3,6 +3,8 @@
  * @author Marián
  */
 
+import FunctionSettings from './FunctionSettings.component';
+
 export default class FunctionsBar extends React.Component
 {
     constructor( props )
@@ -14,6 +16,11 @@ export default class FunctionsBar extends React.Component
     {
         this.props.onRefresh( this.props.week );
     }
+
+    onSettingsClick( e )
+    {
+        this.refs.functionSettings.toggle();
+    }
     /**
      * Renders the component
      */
@@ -22,7 +29,11 @@ export default class FunctionsBar extends React.Component
         return <div className="functions-bar">
             <a href="#" className="refresh-function" onClick={this.onRefresh.bind(this)}><i className="fa fa-refresh"></i></a>
             <a href="#" className="flags-function"><i className="fa fa-flag"></i></a>
+            <a href="#" className="settings-function" onClick={this.onSettingsClick.bind(this)}><i className="fa fa-cog"></i></a>
             <a href="#" className="search-function"><i className="fa fa-search"></i></a>
+            <div className="functions">
+                <FunctionSettings ref="functionSettings" />
+            </div>
         </div>;
     }
 }
