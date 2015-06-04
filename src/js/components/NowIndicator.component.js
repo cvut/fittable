26cf796bac0d72f74ef971251b29b7fc2d827f43
Marián Hlaváč <hlavac95@gmail.com>
2015-06-05 00:59:03 +0200

    Show indicator even when there isn't any upcoming event

--- src/js/components/NowIndicator.component.js
+++ src/js/components/NowIndicator.component.js
@@ -20,11 +20,17 @@ export default class Timetable extends React.Component
         var nowpoint = new Moment().diff( new Moment().hour( this.props.timelineStartHour ) );
         var length = nowpoint / this.props.timelineLength;
 
-        var shown = this.props.viewDate.isSame( new Moment(), 'week' ) && length > 0 && this.props.closestEvent !== null;
+        var shown = this.props.viewDate.isSame( new Moment(), 'week' ) && length > 0;
 
-        return <div className={'now-indicator' + ( shown ? '' : ' hide')} ref="rootEl" style={{ width: length + '%' }}>
-            <div className="next">{this.props.closestEvent.name} {new Moment( this.props.closestEvent.startsAt ).fromNow()}</div>
-            <i className="fa fa-chevron-circle-right"></i>
-        </div>;
+        if ( this.props.closestEvent !== null )
+            return <div className={'now-indicator' + ( shown ? '' : ' hide')} ref="rootEl" style={{ width: length + '%' }}>
+                <div className="next">{this.props.closestEvent.name} {new Moment( this.props.closestEvent.startsAt ).fromNow()}</div>
+                <i className="fa fa-chevron-circle-right"></i>
+            </div>;
+        else
+            return <div className={'now-indicator' + ( shown ? '' : ' hide')} ref="rootEl" style={{ width: length + '%' }}>
+                <div className="next"></div>
+                <i className="fa fa-chevron-circle-right"></i>
+            </div>;
     }
 }
