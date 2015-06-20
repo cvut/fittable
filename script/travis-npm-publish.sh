b5b6042d8dccfaee10f676482ad945954c1b2361
Jakub Jirutka <jakub@jirutka.cz>
2015-06-20 23:41:42 +0200

    Move npm settings from .travis.yml to the publish script and simplify it

--- script/travis-npm-publish.sh
+++ script/travis-npm-publish.sh
@@ -4,10 +4,6 @@ pkg-get() {
     node -p "require('./package.json').$1"
 }
 
-pkg-repo-slug() {
-    pkg-get "repository.url.split('/').splice(-2).join('/')"
-}
-
 is-released() {
     res="$(npm view $1@$2 version 2>/dev/null | head -n 1)"
     [ $? == 0 ] && [ "$res" == "$2" ]; return $?
@@ -18,8 +14,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "This is a pull request, skipping deploy."; exit 0
 fi
 
-if [ "$TRAVIS_REPO_SLUG" != "$(pkg-repo-slug)" ]; then
-    echo "This is a fork, skipping deploy."; exit 0
+if [ -z "$NPM_TOKEN" ]; then
+    echo "NPM_TOKEN is not set, skipping deploy."; exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
@@ -29,6 +25,10 @@ fi
 if [ "$TRAVIS_BUILD_NUMBER.1" != "$TRAVIS_JOB_NUMBER" ]; then
     echo "This is not a first job of the build, skipping deploy."; exit 0
 fi
+
+
+npm set registry https://repository.fit.cvut.cz/npm
+npm set "//repository.fit.cvut.cz/:_authToken=${NPM_TOKEN}"
 
 version=$(pkg-get version)
 if is-released $(pkg-get name) $version; then
