416bd1f40a58641d70653e4439e39f739ef63fd6
Jan Vlnas <git@jan.vlnas.cz>
2015-06-22 21:11:22 +0200

    Remove autoprefixer-rails

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@
 source "https://rubygems.org"
 
 gem 'compass'
-gem 'autoprefixer-rails'
 gem 'sass-globbing'
--- config.rb
+++ config.rb
@@ -1,5 +1,4 @@
 require 'sass-globbing'
-require 'autoprefixer-rails'
 
 add_import_path "node_modules/foundation-sites/scss"
 add_import_path "node_modules"
@@ -10,19 +9,3 @@ css_dir = 'src/css'
 sass_dir = 'src/scss'
 images_dir = 'src/img'
 fonts_dir = 'src/fonts'
-
-# https://github.com/postcss/autoprefixer#compass
-on_stylesheet_saved do |file|
-  css = File.read(file)
-  map = file + '.map'
-  if File.exists? map
-    result = AutoprefixerRails.process(css,
-      from: file,
-      to:   file,
-      map:  { prev: File.read(map), inline: false })
-    File.open(file, 'w') { |io| io << result.css }
-    File.open(map,  'w') { |io| io << result.map }
-  else
-    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
-  end
-end
