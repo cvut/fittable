bbe9a9ad79936f29cf7a526ee1d7879a8a02c914
Jan Vlnas <git@jan.vlnas.cz>
2015-06-23 16:03:12 +0200

    Fix functionality and styling of WeekSwitcher

--- src/js/components/WeekSwitcher.component.js
+++ src/js/components/WeekSwitcher.component.js
@@ -27,9 +27,9 @@ export default class WeekSwitcher extends Toggleable
         let semesterName = this.props.semester;
         let viewMoment = this.viewDateMoment();
 
-        return <div className="row selector semester-selector collapse">
+        return <div className="row weeksw-selector weeksw-semester-selector collapse">
                     <div className="column small-3 gr-go">
-                        <button type="button" onClick={this.props.onDateChange.bind( this, viewMoment().subtract( 6, 'months' ), null )}>
+                        <button type="button" className="gr-go-btn" onClick={this.props.onDateChange.bind( this, viewMoment().subtract( 6, 'months' ), null )}>
                         <i className="fa fa-chevron-left"></i>
                         </button>
                     </div>
@@ -37,7 +37,7 @@ export default class WeekSwitcher extends Toggleable
                         {semesterName}
                     </div>
                     <div className="column small-3 gr-go">
-                        <button type="button" onClick={this.props.onDateChange.bind( this, viewMoment().add( 6, 'months' ), null )}>
+                        <button type="button" className="gr-go-btn" onClick={this.props.onDateChange.bind( this, viewMoment().add( 6, 'months' ), null )}>
                         <i className="fa fa-chevron-right"></i>
                         </button>
                     </div>
@@ -47,9 +47,9 @@ export default class WeekSwitcher extends Toggleable
     renderMonthSelector() {
         let viewMoment = this.viewDateMoment();
 
-        return <div className="row selector month-selector collapse">
+        return <div className="row weeksw-selector weeksw-month-selector collapse">
                 <div className="column small-3 gr-go">
-                    <button type="button" onClick={this.props.onDateChange.bind( this, viewMoment().subtract( 1, 'months' ), null )}>
+                    <button type="button" className="gr-go-btn" onClick={this.props.onDateChange.bind( this, viewMoment().subtract( 1, 'months' ), null )}>
                     <i className="fa fa-chevron-left"></i>
                     </button>
                 </div>
@@ -57,7 +57,7 @@ export default class WeekSwitcher extends Toggleable
                     {viewMoment().format( 'MMMM' )}
                 </div>
                 <div className="column small-3 gr-go">
-                    <button type="button" onClick={this.props.onDateChange.bind( this, viewMoment().add( 1, 'months' ), null )}>
+                    <button type="button" className="gr-go-btn" onClick={this.props.onDateChange.bind( this, viewMoment().add( 1, 'months' ), null )}>
                     <i className="fa fa-chevron-right"></i>
                     </button>
                 </div>
@@ -72,10 +72,10 @@ export default class WeekSwitcher extends Toggleable
         let viewMoment = this.viewDateMoment();
         var weeks = [ [], [], [], [], [], [], [] ], moments = [ [], [], [], [], [], [], [] ];
         var monthEnd = viewMoment().endOf( 'month' ).endOf( 'isoWeek' );
-        var weeki = 0, activeWeekIdx = -1;
+        var lastWeekIndex = 0, activeWeekIdx = -1;
 
         // Create weeks of month array
-        for ( var i = viewMoment().startOf( 'month' ).startOf( 'isoWeek' ); i.isBefore( monthEnd ); i.add( 1, 'day' ) )
+        for ( var i = viewMoment().startOf( 'month' ).startOf( 'isoWeek' ), weeki = 0; i.isBefore( monthEnd ); i.add( 1, 'day' ) )
         {
             weeks[ weeki ].push( i.date() );
             if ( i.isoWeekday() === 7 )
@@ -87,10 +87,11 @@ export default class WeekSwitcher extends Toggleable
             if ( i.isSame( this.props.viewDate, 'week' ) ) {
                 activeWeekIdx = weeki;
             }
+            lastWeekIndex = weeki;
         }
 
         function activeWeekClass(week) {
-            if( activeWeekIdx === (weeks.indexOf( week ) + 1) ) {
+            if( activeWeekIdx === (weeks.indexOf( week )) ) {
                 return 'active-week';
             }
             else {
@@ -104,7 +105,9 @@ export default class WeekSwitcher extends Toggleable
          */
         function dayClass(week, day) {
             let weekIndex = weeks.indexOf( week );
-            if( (day > 7 && weekIndex === 0) || (day < 7 && weekIndex === (week - 1)) ) {
+
+            // if the day is in first week AND larger than > 7 OR last week AND smaller than 7
+            if( (weekIndex === 0 && day > 7) || ( (weekIndex === (lastWeekIndex - 1)) && day < 7 ) ) {
                 return 'in-other';
             }
             else {
@@ -112,21 +115,27 @@ export default class WeekSwitcher extends Toggleable
             }
         }
 
-        return <div className="week-switcher hide" ref="rootEl">
+        function switchWeek(week) {
+            var weekIndex = weeks.indexOf( week );
+            var moment = moments[weekIndex];
+            this.props.onDateChange.call(this, moment);
+        }
+
+        return <div className="weeksw hide" ref="rootEl">
             {this.renderSemesterSelector()}
             {this.renderMonthSelector()}
             {weeks.map( function( week ) {
-                return <div className={'row selector week-selector ' + activeWeekClass(week) } key={weeks.indexOf( week )}>
+                return <div className={'row weeksw-selector weeksw-week-selector ' + activeWeekClass(week) } key={weeks.indexOf( week )}>
                     <div className="column small-12">
                         {week.map( function( day ) {
-                            return <button type="button" onClick={this.props.onDateChange.bind( this, moments[ weeks.indexOf( week ) ] )} key={weeks.indexOf( week ) + '-' + day}>
-                                    <div className={'day' + dayClass(week, day)}>{day}</div>
-                                </button>;
+                            return <button type="button" className={'weeksw-day ' + dayClass(week, day)}
+                                           onClick={switchWeek.bind(this, week)} key={weeks.indexOf( week ) + '-' + day}>
+                                    {day}
+                                    </button>;
                         }.bind( this ) ) }
                     </div>
                 </div>;
             }.bind( this ) ) }
-
         </div>;
     }
 }
