d762c3520f6798af3d4df2ebb74bfdb12a76b9a8
Jan Vlnas <git@jan.vlnas.cz>
2015-08-27 19:01:52 +0200

    Introduce reducer for settings

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,27 @@
-export sample from './sample'
+import { combineReducers } from 'redux'
+import { merge } from 'ramda'
+
+import { SETTINGS_CHANGE } from '../constants/actionTypes'
+
+const initialSettings = {
+  locale: 'cs',
+  layout: 'horizontal',
+  eventsColors: false,
+  fullWeek: false,
+  facultyGrid: false,
+}
+
+function settings (state = initialSettings, action) {
+  switch (action.type) {
+    case SETTINGS_CHANGE:
+      return merge(state, action.settings)
+    default:
+      return state
+  }
+}
+
+const fittableApp = combineReducers({
+  settings,
+})
+
+export default fittableApp
---
+++ test/reducers/index.test.js
@@ -0,0 +1,17 @@
+import test from 'blue-tape'
+import R from 'ramda'
+import { SETTINGS_CHANGE } from '../../src/constants/actionTypes'
+import reducer from '../../src/reducers'
+
+test('Initial state', t => {
+  const result = reducer(undefined, {type: 'FAUX_ACTION'})
+  t.ok(R.is(Object, result.settings), 'Missing property settings in initial state')
+  t.end()
+})
+
+test('Settings change', t => {
+  const settings = reducer(undefined, {type: SETTINGS_CHANGE, settings: {locale: 'en', layout: 'vertical'}}).settings
+  t.equal(settings.locale, 'en')
+  t.equal(settings.layout, 'vertical')
+  t.end()
+})
