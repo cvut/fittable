9038dfe39e2bed07b0d41cad6e5f9fbe31ddb960
Jan Vlnas <git@jan.vlnas.cz>
2015-09-03 17:44:50 +0200

    Implement additional date functions

--- src/date.js
+++ src/date.js
@@ -1,3 +1,35 @@
-// import { moment } from 'moment'
+import moment from 'moment'
+import { curry } from 'ramda'
 
 export const now = () => Object.freeze(new Date())
+
+export function isoDate (date) {
+  return moment(date).format('YYYY-MM-DD')
+}
+
+function momentWeekRange (date) {
+  const m = moment(date)
+
+  const weekStart = m.clone().startOf('isoWeek')
+  const weekEnd = m.clone().endOf('isoWeek')
+
+  return [weekStart, weekEnd]
+}
+
+export function weekRange (date) {
+  return momentWeekRange(date).map(m => m.toDate())
+}
+
+export function workWeekRange (date) {
+  const [weekStart, weekEnd] = momentWeekRange(date)
+
+  return [weekStart, weekEnd.subtract(2, 'days')].map(m => m.toDate())
+}
+
+export function isoWeekRange (date) {
+  return weekRange(date).map(d => isoDate(d))
+}
+
+export const shiftDate = curry(function shiftDate (baseDate, kind, offset) {
+  return moment(baseDate).add(offset, kind).toDate()
+})
--- test/date.test.js
+++ test/date.test.js
@@ -1,11 +1,80 @@
 import test from 'blue-tape'
 import tk from 'timekeeper'
 import * as date from '../src/date'
+
 test('now', (t) => {
   tk.freeze()
   const currentDate = new Date()
   t.assert(Object.isFrozen(date.now()), 'returns a frozen object')
-  t.deepEqual(currentDate.toString(), date.now().toString())
+  t.deepEqual(date.now().toString(), currentDate.toString(), 'generates current date')
   tk.reset()
+  t.end()
+})
+
+test('isoDate', (t) => {
+  const expectedDate = '2015-09-03'
+  const today = new Date(`${expectedDate}T00:00:00+0200`)
+
+  t.equal(date.isoDate(today), expectedDate, 'generates ISO date in a correct timezone')
+  t.end()
+})
+
+test('weekRange', (t) => {
+  const today = new Date('2015-09-03')
+  const start = new Date('2015-08-31T00:00:00+0200').toString()
+  const end = new Date('2015-09-06T23:59:59+0200').toString()
+  const expectedRange = [start, end]
+
+  const actualRange = date.weekRange(today).map(d => d.toString())
+
+  t.deepEqual(actualRange, expectedRange)
+
+  t.end()
+})
+
+test('workWeekRange', (t) => {
+  const today = new Date('2015-09-03')
+  const start = new Date('2015-08-31T00:00:00+0200').toString()
+  const end = new Date('2015-09-04T23:59:59+0200').toString()
+  const expectedRange = [start, end]
+
+  const actualRange = date.workWeekRange(today).map(d => d.toString())
+
+  t.deepEqual(actualRange, expectedRange)
+
+  t.end()
+})
+
+test('isoWeekRange', (t) => {
+  const today = new Date('2015-09-03')
+
+  const start = '2015-08-31'
+  const end = '2015-09-06'
+  const expectedRange = [start, end]
+
+  const actualRange = date.isoWeekRange(today)
+
+  t.deepEqual(actualRange, expectedRange)
+
+  t.end()
+})
+
+test('shiftDate', t => {
+  const today = new Date('2015-09-03')
+  const shiftDateForToday = date.shiftDate(today)
+  t.is(typeof shiftDateForToday, 'function', 'is curried')
+
+  const testParams = [
+    {args: ['week', 1], expected: '2015-09-10'},
+    {args: ['weeks', -2], expected: '2015-08-20'},
+    {args: ['months', +3], expected: '2015-12-03'},
+    {args: ['months', -12], expected: '2014-09-03'},
+  ]
+
+  testParams.forEach(({args, expected}) => {
+    const actual = shiftDateForToday(...args)
+    t.equal(date.isoDate(actual), expected, `shifts by ${args[1]} ${args[0]}`)
+  })
+
   t.end()
 })
