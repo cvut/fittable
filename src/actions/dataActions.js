7224d5efa90302ea7a92ff95c44c980a968b4263
Jan Vlnas <git@jan.vlnas.cz>
2015-09-07 17:11:26 +0200

    Implement basic dataActions

---
+++ src/actions/dataActions.js
@@ -0,0 +1,35 @@
+import { EVENTS_REQUEST, EVENTS_RESPONSE } from '../constants/actionTypes'
+import { isoWeekRange } from '../date'
+
+function startEventsRequest () {
+  return {
+    type: EVENTS_REQUEST,
+  }
+}
+
+function receiveEvents (events, linkNames) {
+
+  return {
+    type: EVENTS_RESPONSE,
+    payload: {
+      events,
+      linkNames,
+    },
+  }
+
+}
+
+export function fetchEvents (dataCallback, weekDate) {
+  const [dateFrom, dateTo] = isoWeekRange(weekDate)
+
+  return function (dispatch) {
+    // First dispatch: inform state that loading is going on
+    dispatch(startEventsRequest())
+
+    dataCallback(dateFrom, dateTo, function (events, linkNames) {
+      // We have (hopefully) received data by now
+      // FIXME: add error handling
+      dispatch(receiveEvents(events, linkNames))
+    })
+  }
+}
---
+++ test/actions/dataActions.test.js
@@ -0,0 +1,60 @@
+import test from 'blue-tape'
+import { spy } from 'sinon'
+import { EVENTS_REQUEST, EVENTS_RESPONSE } from '../../src/constants/actionTypes'
+import * as actions from '../../src/actions/dataActions'
+
+test('fetchEvents() executes a given callback with a week range', t => {
+  const expectedFrom = '2015-09-07'
+  const expectedTo = '2015-09-13'
+  const today = new Date('2015-09-09')
+
+  const dataCallback = (actualFrom, actualTo, cb) => {
+    t.equal(actualFrom, expectedFrom, 'callback receives dateFrom string for a week start')
+    t.equal(actualTo, expectedTo, 'callback receives dateTo string for a week end')
+    t.equal(typeof cb, 'function', 'callback receives a callback for response')
+    t.end()
+  }
+
+  const thunk = actions.fetchEvents(dataCallback, today)
+  const dispatch = () => {}
+
+  t.equal(typeof thunk, 'function', 'fetchEvents returns a thunk function immediately')
+
+  thunk(dispatch)
+})
+
+test('fetchEvents() dispatch', t => {
+  const today = new Date('2015-09-09')
+  const dispatch = spy()
+
+  const responseData = [
+    ['event', 'event'],
+    {
+      cs: {
+        teachers: {vomackar: 'Karel Vomáčka'},
+      },
+    },
+  ]
+
+  const callback = (from, to, cb) => cb(...responseData)
+  const thunk = actions.fetchEvents(callback, today)
+  thunk(dispatch)
+
+  const expectedCalls = 2
+  t.equal(dispatch.callCount, expectedCalls, `dispatch has been called ${expectedCalls} times`)
+
+  t.test('fetchEvents() first dispatch', st => {
+    const expectedArg = {type: EVENTS_REQUEST}
+    const [actualArg,] = dispatch.firstCall.args
+    st.deepEqual(actualArg, expectedArg, 'dispatches an EVENTS_REQUEST')
+    st.end()
+  })
+
+  t.test('fetchEvents() second dispatch', st => {
+    const [events, linkNames] = responseData
+    const expectedArg = {type: EVENTS_RESPONSE, payload: {events, linkNames}}
+    const [actualArg,] = dispatch.secondCall.args
+    st.deepEqual(actualArg, expectedArg, 'dispatches an EVENTS_RESPONSE')
+    st.end()
+  })
+})
