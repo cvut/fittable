003559335170014bcd96495004f69ea5455824ad
Marián Hlaváč <hlavac95@gmail.com>
2015-09-25 22:05:05 +0200

    New breakpoints
    small, medium, large @ 480, 800, inf

---
+++ src/constants/screenSizes.js
@@ -0,0 +1,6 @@
+export const SMALL_SCREEN = 'small'
+export const MEDIUM_SCREEN = 'medium'
+export const LARGE_SCREEN = 'large'
+
+export const SMALL_SCREEN_BREAKPOINT = 480
+export const MEDIUM_SCREEN_BREAKPOINT = 800
