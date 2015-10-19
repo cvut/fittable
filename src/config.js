28aec25cac73d1ed341ba01a7d790e905a56c043
Jan Vlnas <git@jan.vlnas.cz>
2015-10-19 18:46:42 +0200

    Accept SIRIUS_GLOBAL_URL as a global variable

--- src/config.js
+++ src/config.js
@@ -4,6 +4,10 @@ let SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/staging/api/v1/'
 if (process.env.SIRIUS_TARGET === 'production') {
   SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/api/v1/'
 }
+// Allow final override from outside context (eg. HTML file)
+if (global.SIRIUS_BASE_URL) {
+  SIRIUS_BASE_URL = global.SIRIUS_BASE_URL
+}
 
 export {
   SIRIUS_BASE_URL,
