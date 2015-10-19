520c8cda92fcf0b1e2b0b14325fbcb2a3f6a34a5
Jan Vlnas <git@jan.vlnas.cz>
2015-10-19 18:54:00 +0200

    Fix tests for clientActions and clientReducer

--- test/actions/clientActions.test.js
+++ test/actions/clientActions.test.js
@@ -2,22 +2,25 @@ import test from 'blue-tape'
 import { spy } from 'sinon'
 import { CLIENT_CHANGE } from '../../src/constants/actionTypes'
 import * as actions from '../../src/actions/clientActions'
+import { SMALL_SCREEN, SMALL_SCREEN_BREAKPOINT, MEDIUM_SCREEN, MEDIUM_SCREEN_BREAKPOINT, LARGE_SCREEN } from '../../src/constants/screenSizes'
+
 
 test('detectScreenSize()', t => {
   const thunk = actions.detectScreenSize()
 
   const dispatch = spy()
-  const getState = () => { return {client: {smallScreen: true}} }
+  const getState = () => { return {client: {screenSize: SMALL_SCREEN}} }
+  const getSecondState = () => { return {client: {screenSize: LARGE_SCREEN}} }
 
   // stubbing global window object
-  global.window = {innerWidth: 800}
+  global.window = {innerWidth: 1280}
   thunk(dispatch, getState)
-  global.window = {innerWidth: 500}
-  thunk(dispatch, getState)
+  global.window = {innerWidth: 1290}
+  thunk(dispatch, getSecondState)
 
   t.equal(dispatch.callCount, 1, 'dispatches only once if the current state is different')
 
-  const expectedArg = {type: CLIENT_CHANGE, payload: {smallScreen: false}}
+  const expectedArg = {type: CLIENT_CHANGE, payload: {screenSize: LARGE_SCREEN}}
   const [actualArg,] = dispatch.firstCall.args
   t.deepEqual(actualArg, expectedArg, 'dispatches CLIENT_CHANGE with smallScreen payload')
 
--- test/reducers/clientReducer.test.js
+++ test/reducers/clientReducer.test.js
@@ -1,11 +1,13 @@
 import test from 'blue-tape'
 import { CLIENT_CHANGE } from '../../src/constants/actionTypes'
 import reducer from '../../src/reducers/clientReducer'
+import { SMALL_SCREEN, MEDIUM_SCREEN } from '../../src/constants/screenSizes'
+
 
 test('client reducer initial state', t => {
   const actual = reducer(undefined, {type: null})
   const expected = {
-    smallScreen: true,
+    screenSize: SMALL_SCREEN,
   }
   t.deepEqual(actual, expected, 'small screen is considered by default')
   t.end()
@@ -13,12 +15,12 @@ test('client reducer initial state', t => {
 
 test('client reducer CLIENT_CHANGE action', t => {
   const state = {
-    smallScreen: true,
+    screenSize: SMALL_SCREEN,
   }
   const expected = {
-    smallScreen: false,
+    screenSize: MEDIUM_SCREEN,
   }
-  const actual = reducer(state, {type: CLIENT_CHANGE, payload: {smallScreen: false}})
+  const actual = reducer(state, {type: CLIENT_CHANGE, payload: {screenSize: MEDIUM_SCREEN}})
   t.deepEqual(actual, expected, 'small screen is considered by default')
   t.end()
 })
