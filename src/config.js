0f139ccf53c99df608ed716fb61d277d880c41e5
Jan Vlnas <git@jan.vlnas.cz>
2015-10-19 18:16:02 +0200

    Simplify env.SIRIUS_TARGET behaviour

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const TZ = 'Europe/Prague'
 
 let SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/staging/api/v1/'
-if (process.env.NODE_ENV === 'production' || process.env.SIRIUS_TARGET === 'production') {
+if (process.env.SIRIUS_TARGET === 'production') {
   SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/api/v1/'
 }
 
