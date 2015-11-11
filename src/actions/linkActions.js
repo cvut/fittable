0cf91e1a88669aab39f587a4f2c403d5496a4099
Jan Vlnas <git@jan.vlnas.cz>
2015-11-11 19:16:22 +0100

    Pass full calendar object to linkActions.changeCalendar

--- src/actions/linkActions.js
+++ src/actions/linkActions.js
@@ -10,19 +10,15 @@ function entityType (type) {
   return ENTITY_TYPE_PLURAL[type] || type
 }
 
-export function calendarUrl (type, id, date = null) {
+export function calendarUrl ({id, date, type = null}) {
   const urlType = entityType(type)
   const dateQuery = date ? `?date=${date}` : ''
 
   return `${urlType}/${id}${dateQuery}`
 }
 
-export function changeCalendar (type, id) {
+export function changeCalendar (calendar) {
   // XXX: state object is required by history
   //   https://github.com/rackt/history/blob/master/docs/GettingStarted.md#navigation
-  return pushState(null, calendarUrl(type, id))
-}
-
-export function changeViewDate (newDate) {
-
+  return pushState(null, calendarUrl(calendar))
 }
--- test/actions/linkActions.test.js
+++ test/actions/linkActions.test.js
@@ -1,19 +1,26 @@
 import test from 'blue-tape'
 import * as actions from '../../src/actions/linkActions'
 
+const CALENDAR = {type: 'course', id: 'MI-RUB', date: '2015-10-11'}
+
 test('calendarUrl()', t => {
   t.test('calendarUrl() with all parameters', st => {
     st.plan(1)
     const expected = 'courses/MI-RUB?date=2015-10-11'
-    const actual = actions.calendarUrl('course', 'MI-RUB', '2015-10-11')
+    const actual = actions.calendarUrl(CALENDAR)
 
     st.equal(actual, expected, 'generates URL for singular entity type')
   })
 
   t.test('calendarUrl() without date', st => {
     st.plan(1)
+    const calendar = {
+      type: 'person',
+      id: 'vomackar',
+      date: null,
+    }
     const expected = 'people/vomackar'
-    const actual = actions.calendarUrl('person', 'vomackar')
+    const actual = actions.calendarUrl(calendar)
 
     st.equal(actual, expected, 'generates URL without a date')
   })
@@ -22,9 +29,9 @@ test('calendarUrl()', t => {
 })
 
 test('changeCalendar()', t => {
-  const actual = actions.changeCalendar('person', 'MI-RUB')
+  const actual = actions.changeCalendar(CALENDAR)
   const expectedPayload = {
-    args: [null, 'people/MI-RUB'],
+    args: [null, 'courses/MI-RUB?date=2015-10-11'],
     method: 'pushState',
   }
   t.deepEqual(actual.payload, expectedPayload)
