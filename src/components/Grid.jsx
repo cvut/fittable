8100f9aa5c1b1e801251f2786521537231edfb62
Marián Hlaváč <hlavac95@gmail.com>
2015-11-16 13:56:55 +0100

    Rewrite the line generator in Grid as a pure function

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,6 +3,7 @@
  */
 
 import React, { PropTypes } from 'react'
+import R from 'ramda'
 
 const propTypes = {
   horizontal: PropTypes.bool,
@@ -11,56 +12,40 @@ const propTypes = {
   color: PropTypes.string.isRequired,
 }
 
-const PATTERN_ID = 'Grid-pattern'
-
 class Grid extends React.Component {
 
-  getPattern () {
-    const hourOffset = this.props.timelineOffset * this.props.hourLength
+  createLine (type, hours, offset, color) {
+    const defaults = {
+      x1: '0',
+      y1: '0',
+      x2: '100%',
+      y2: '100%',
+      stroke: color,
+      strokeWidth: 1,
+    }
 
-    if (this.props.horizontal) {
-      return (
-        <pattern id={PATTERN_ID} x={hourOffset} y="0" width={this.props.hourLength} height="100%">
-          <line x1="0" y1="0" x2="0" y2="100%" stroke={this.props.color} strokeWidth="1" />
-        </pattern>
-      )
-    } else {
-      return (
-        <pattern id={PATTERN_ID} x="0" y={hourOffset} width="100%" height={this.props.hourLength}>
-          <line x1="0" y1="0" x2="100%" y2="0" stroke={this.props.color} strokeWidth="1" />
-        </pattern>
-      )
+    return (n) => {
+      const pos = ((n + offset) / hours * 100) + '%'
+
+      if (type === 'vertical') {
+        return <line {...defaults} y1={pos} y2={pos} />
+      } else {
+        return <line {...defaults} x1={pos} x2={pos} />
+      }
     }
   }
 
   getLines () {
-    let lines = []
+    const hours = Math.ceil(this.props.hours)
+    const type = this.props.horizontal ? 'horizontal' : 'vertical'
 
-    for (let i = 0; i < this.props.hours; i++) {
-      const pos = i / this.props.hours * 100 + (this.props.offset * 100 / this.props.hours)
-
-      if (this.props.horizontal) {
-        lines.push(<line x1={pos + '%'} y1="0" x2={pos + '%'} y2="100%" stroke={this.props.color} strokeWidth="1"/>)
-      } else {
-        lines.push(<line x1="0" y1={pos + '%'} x2="100%" y2={pos + '%'} stroke={this.props.color} strokeWidth="1"/>)
-      }
-    }
-
-    return lines
-  }
-
-  // XXX:You need actually either document's name or a full URL
-  //     to resolve the url() reference.
-  //     http://stackoverflow.com/q/18259032/240963
-  //     Yes, SVG is Magical!
-  getPatternUrl () {
-    return `${global.location.href}#${PATTERN_ID}`
+    return R.times(this.createLine(type, hours, this.props.offset, this.props.color), hours)
   }
 
   render () {
     return (
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
-        {this.getLines()}
+        {this.getLines(this)}
       </svg>
     )
   }
