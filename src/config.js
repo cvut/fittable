d95246470a49888efa7fb5ab3033d5cf685029ee
Marián Hlaváč <hlavac95@gmail.com>
2015-11-23 14:23:03 +0100

    Search semesters by faculty id

--- src/config.js
+++ src/config.js
@@ -1,3 +1,20 @@
 export const TZ = 'Europe/Prague'
 
 export const SIRIUS_PROXY_PATH = global.SIRIUS_PROXY_PATH || process.env.SIRIUS_PROXY_PATH
+
+let SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/staging/api/v1/'
+if (process.env.SIRIUS_TARGET === 'production') {
+  SIRIUS_BASE_URL = 'https://sirius.fit.cvut.cz/api/v1/'
+}
+// Allow final override from outside context (eg. HTML file)
+if (global.SIRIUS_BASE_URL) {
+  SIRIUS_BASE_URL = global.SIRIUS_BASE_URL
+}
+
+// Faculty profile number
+export const FACULTY_ID = 18000
+
+export {
+  SIRIUS_BASE_URL,
+  TZ
+}
