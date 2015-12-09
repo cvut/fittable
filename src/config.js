48ef57b90e8567574199dddb5b6cee78b6b039b0
Jan Vlnas <git@jan.vlnas.cz>
2015-12-09 17:44:47 +0100

    Allow SIRIUS_PROXY_PATH override by global var

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const TZ = 'Europe/Prague'
 
-export const SIRIUS_PROXY_PATH = process.env.SIRIUS_PROXY_PATH
+export const SIRIUS_PROXY_PATH = global.SIRIUS_PROXY_PATH || process.env.SIRIUS_PROXY_PATH
