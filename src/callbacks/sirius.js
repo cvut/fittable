ae6fc9b6fc1b8ae62674577f9c730b291a4ce595
Jan Vlnas <git@jan.vlnas.cz>
2015-12-10 15:11:37 +0100

    Cleanup sirius callbacks
    - Remove obsolete callbacks and globals
    - Centralize requests handling

--- src/callbacks/sirius.js
+++ src/callbacks/sirius.js
@@ -17,91 +17,54 @@ import { SIRIUS_PROXY_PATH } from '../config'
 const emptyObject = (obj) => R.is(Object, obj) && R.pipe(R.keys, R.propEq('length', 0))
 
 /**
- * Fittable widget instance
- */
-var f
-
-/**
- * Main application parameter, specifying the view
- * Choices are: person, room, course
- * @type {string}
- */
-var view = 'person'
-
-/**
- * Parameter for the selected view. Used for specifying the room or course.
- * @type {string}
- */
-var parameter = ''
-
-/**
  * Logged user's username.
  */
-var username = ReactCookie.load('oauth_username')
+const username = ReactCookie.load('oauth_username')
 
 /**
  * URL of proxy for Sirius API
  * @type {string}
  */
-var siriusAPIUrl = `${getBaseUri()}${SIRIUS_PROXY_PATH}/`
-
-/**
- * Currently selected application language setting
- * @type {string}
- */
-var appLocale = 'cs'
-
-
-var viewPaths = {
-  course: 'courses',
-  person: 'people',
-  room: 'rooms',
-}
+const siriusAPIUrl = `${getBaseUri()}${SIRIUS_PROXY_PATH}/`
 
 // TODO: This should be removed!
-var defaultLimit = 200
+const defaultLimit = 200
 
-var STATUS_ERROR_TYPES = {
+const STATUS_ERROR_TYPES = {
   0: 'connection',
   401: 'unauthorized',
   403: 'access',
   404: 'notfound',
-  500: 'servererror'
+  500: 'servererror',
 }
+
 function generateError (status, message = 'No message specified') {
   const error = new Error(message)
   if (status in STATUS_ERROR_TYPES) {
     error.type = STATUS_ERROR_TYPES[status]
-    if (error.type === 'notfound' && view === 'person' && parameter === username) {
+    /* if (error.type === 'notfound' && view === 'person' && parameter === username) {
       error.type = 'own' + error.type
-    }
+    } */
   } else {
     error.type = 'generic'
   }
   return error
 }
 
-
 /**
  * @return {string} a base URI (aka relative URL root) of this site with
  * trailing slash omitted.
  */
 function getBaseUri () {
-  var baseUri = document.baseURI
+  let baseUri = document.baseURI
 
   // IE doesn't support baseURI, workaround
   if (typeof baseURI === 'undefined') {
-    var baseTag = document.getElementsByTagName("base")[0]
+    const baseTag = document.getElementsByTagName('base')[0]
     baseUri = baseTag.href
   }
 
   return URL.parse(baseUri).pathname.replace(/\/$/, '')
-}
-
-function changeView (newView, newParam) {
-  // Set new view
-  view = newView
-  parameter = newParam
 }
 
 function isUserLoggedIn () {
@@ -111,12 +74,15 @@ function isUserLoggedIn () {
   )
 }
 
-function makeRequest (parameters) {
-  var requestUrl = siriusAPIUrl
+function makeRequest (parameters = '', requestHandler) {
+  const requestUrl = `${siriusAPIUrl}${parameters}`
 
-  requestUrl += parameters
-
-  var request = new XMLHttpRequest()
+  const request = new XMLHttpRequest()
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      requestHandler(request)
+    }
+  }
   request.open('GET', encodeURI(requestUrl), true)
   request.send(null)
 
@@ -138,10 +104,7 @@ function dataCallback ({calendarType, dateFrom, dateTo, calendarId}, callback) {
     `?from=${dateFrom}&to=${dateTo}&limit=${defaultLimit}` +
     `&include=courses,teachers,schedule_exceptions&deleted=true`
 
-  // Make the request
-  const request = makeRequest(path)
-
-  request.onreadystatechange = function (callback, fittable) {
+  function requestHandler (request) {
     if (request.readyState === XMLHttpRequest.DONE) {
       if (request.status === 200) {
         // Request successful
@@ -248,7 +211,9 @@ function dataCallback ({calendarType, dateFrom, dateTo, calendarId}, callback) {
         callback(generateError(request.status, 'dataCallback failed'))
       }
     }
-  }.bind(null, callback, f)
+  }
+
+  makeRequest(path, requestHandler)
 }
 
 /**
@@ -256,39 +221,28 @@ function dataCallback ({calendarType, dateFrom, dateTo, calendarId}, callback) {
  * @param query
  * @param callback
  */
-var searchCallback = function (query, callback) {
-
-  // Construct the request URL
-  var request = makeRequest('search?q=' + query + '&limit=' + defaultLimit)
-
-  request.onreadystatechange = function (callback, fittable) {
+function searchCallback (query, callback) {
+  function requestHandler (request) {
     if (request.readyState === XMLHttpRequest.DONE) {
       if (request.status === 200) {
         // Request successful
-        var ajaxresult = JSON.parse(request.responseText)
+        const ajaxresult = JSON.parse(request.responseText)
 
         // Send data to fittable
         callback(ajaxresult.results)
       } else {
         // Request failed
-        fittable.onError(generateError(request.status).type)
+        // fittable.onError(generateError(request.status).type)
+        console.error(generateError(request.status))
       }
     }
-  }.bind(null, callback, f)
+  }
+
+  makeRequest(`search?q=${query}&limit=${defaultLimit}`, requestHandler)
 }
 
-var dateChangeCallback = function (newdate, newsemester) {
-  console.log(newdate, newsemester)
-  // Display semester name in subheading
-  updateTitle(null, newsemester)
-}
-
-var semesterDataCallback = function (callback) {
-
-  // Construct the request URL
-  var request = makeRequest('semesters?limit=' + defaultLimit)
-
-  request.onreadystatechange = function (callback, fittable) {
+function semesterDataCallback (callback) {
+  function requestHandler (request) {
     if (request.readyState === XMLHttpRequest.DONE) {
       if (request.status === 200) {
         // Request successful
@@ -318,7 +272,7 @@ var semesterDataCallback = function (callback) {
             hourDuration: semester.hour_duration,
             breakDuration: 15,  // FIXME: replace this and that two below with semester.hourStarts
             dayStartsAtHour: 7.5,
-            dayEndsAtHour: 21.25
+            dayEndsAtHour: 21.25,
           }
         }
 
@@ -326,42 +280,17 @@ var semesterDataCallback = function (callback) {
         callback(semesters)
       } else {
         // Request failed
-        fittable.onError(generateError(request.status).type)
+        // fittable.onError(generateError(request.status).type)
+        console.error(generateError(request.status))
       }
     }
-  }.bind(null, callback, f)
-}
+  }
 
-var settingsChangeCallback = function (settings) {
-
-  // We really don't want to save the user property
-  settings.user = null
-
-  // Update appLocale
-  appLocale = settings.locale
-
-  // Update title and subtitle for possible locale change
-  updateTitle(view === 'person' && parameter === username ?
-    viewNames[appLocale]['personal'] : viewNames[appLocale][view] + ' ' + parameter, null)
-}
-
-/**
- * --- Initialize the widget ---
- */
-
-function viewChangeCallback (view, param) {
-  console.error('viewChangeCallback fired!')
-}
-
-// If the user is not logged in, redirect him to the landing page
-if (!isUserLoggedIn()) {
-  window.location.href = 'landing.html'
+  makeRequest(`semesters?limit=${defaultLimit}`, requestHandler)
 }
 
 function userCallback (cb) {
-  const request = makeRequest(`people/${username}`)
-
-  request.onreadystatechange = (callback, fittable) => {
+  function requestHandler (request) {
     if (request.readyState === XMLHttpRequest.DONE) {
       if (request.status === 200) {
         // Request successful
@@ -387,14 +316,18 @@ function userCallback (cb) {
       }
     }
   }
+
+  makeRequest(`people/${username}`, requestHandler)
+}
+
+// If the user is not logged in, redirect him to the landing page
+if (!isUserLoggedIn()) {
+  window.location.href = 'landing.html'
 }
 
 export {
   dataCallback as data,
   searchCallback as search,
-  dateChangeCallback as dateChange,
   semesterDataCallback as semesterData,
-  settingsChangeCallback as settingsChange,
-  viewChangeCallback as viewChange,
   userCallback as user,
 }
