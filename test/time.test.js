f8282179dbf7aa5e72fd2037f1cac63bcc365223
Jakub Jirutka <jakub@jirutka.cz>
2015-12-16 02:39:06 +0100

    Refactor test for convertSecondsToTime()

--- test/time.test.js
+++ test/time.test.js
@@ -2,20 +2,16 @@ import test from 'blue-tape'
 import * as time from '../src/time'
 
 test('convertSecondsToTime()', t => {
-  [
-    // seconds, [h, m, s]
-    [0, [0, 0, 0]],
-    [1, [0, 0, 1]],
-    [60, [0, 1, 0]],
-    [65, [0, 1, 5]],
-    [3600, [1, 0, 0]],
-    [3601, [1, 0, 1]],
-  ].forEach(([seconds, resultTime]) => {
-    const [h, m, s] = resultTime
+  [//seconds, h, m, s]
+    [0,       0, 0, 0],
+    [1,       0, 0, 1],
+    [60,      0, 1, 0],
+    [65,      0, 1, 5],
+    [3600,    1, 0, 0],
+    [3601,    1, 0, 1],
+  ].forEach(([seconds, h, m, s]) => {
     t.deepEqual(
-      time.convertSecondsToTime(seconds),
-      {h, m, s},
-      `converts correctly ${seconds}s to time`
+      time.convertSecondsToTime(seconds), {h, m, s}, `converts correctly ${seconds}s to time`
     )
   })
 
