d7cac48080076ed2851baf65b814816583ed2b57
Jan Vlnas <git@jan.vlnas.cz>
2016-01-04 16:41:37 +0100

    Add PeriodicUpdate component

---
+++ src/components/PeriodicUpdate.jsx
@@ -0,0 +1,55 @@
+/**
+ * High-order component which periodically updates
+ */
+import { createClass, PropTypes, Children, cloneElement } from 'react'
+import { now } from '../date'
+
+const PeriodicUpdate = createClass({
+  propTypes: {
+    period: PropTypes.number,
+    children: PropTypes.element.isRequired,
+  },
+
+  displayName: 'PeriodicUpdate',
+
+  getDefaultProps () {
+    return {
+      period: 1000,
+    }
+  },
+
+  getInitialState () {
+    return {
+      currentDate: now(),
+      timeoutObject: null,
+    }
+  },
+
+  componentDidMount () {
+    this.setState({ timeoutObject: global.setTimeout(this.updateDate, this.props.period) })
+  },
+
+  componentWillUnmount () {
+    const { timeoutObject } = this.state
+    if (timeoutObject) {
+      global.clearTimeout(timeoutObject)
+    }
+  },
+
+  updateDate () {
+    this.setState({
+      currentDate: now(),
+      timeoutObject: global.setTimeout(this.updateDate, this.props.period),
+    })
+  },
+
+  render () {
+    const { children } = this.props
+    const { currentDate } = this.state
+
+    const child = Children.only(children)
+    return cloneElement(child, { currentDate })
+  },
+})
+
+export default PeriodicUpdate
