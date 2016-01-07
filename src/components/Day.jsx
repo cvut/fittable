f9508cc052cae006cee171d6bb0204661bb6156d
Jan Vlnas <git@jan.vlnas.cz>
2016-01-07 12:36:48 +0100

    Fix Day component

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -6,14 +6,14 @@ import React, { PropTypes } from 'react'
 import moment from 'moment'
 
 const propTypes = {
-  date: PropTypes.instanceOf(moment),
+  date: PropTypes.instanceOf(Date),
   viewDate: PropTypes.instanceOf(Date),
 }
 
 class Day extends React.Component {
 
   render () {
-    const date = this.props.date
+    const date = moment(this.props.date)
     const isToday = date.isSame(moment(), 'days')
     const isSelected = date.isSame(this.props.viewDate, 'days')
     const className = `day ${isToday ? 'active' : ''} ${isSelected ? 'selected' : ''}`
@@ -22,7 +22,7 @@ class Day extends React.Component {
       <div className={className}>
         <div className="label">
           <div className="label-wrap">
-            <span className="day-num">{date.getDate()}</span>
+            <span className="day-num">{date.date()}</span>
             <span className="day-name">{date.format('dddd')}</span>
           </div>
         </div>
