ed2b227f1c91df14b2388bd5b1e41eecfafb6396
Marián Hlaváč <hlavac95@gmail.com>
2016-01-18 11:56:48 +0100

    Rewrite classModifiers() into new className()

---
+++ test/utils/className.test.js
@@ -0,0 +1,42 @@
+import test from 'blue-tape'
+import { apply } from 'ramda'
+import className from '../../src/utils/className'
+
+test('className()', t => {
+  const tests = [
+    {
+      args: ['ComponentName', ['descendants'], ['modifier'], { states: true }],
+      expected: 'ComponentName-descendants ComponentName-descendants--modifier is-states',
+    },
+    {
+      args: ['AwesomeComponent', ['heading'], null, { blinking: false }],
+      expected: 'AwesomeComponent-heading',
+    },
+    {
+      args: ['AwesomeComponent', ['footer'], ['darker'], { blinking: true }],
+      expected: 'AwesomeComponent-footer AwesomeComponent-footer--darker is-blinking',
+    },
+    {
+      args: ['MenuBar', null, ['lighter'], { blinking: true, visible: true }],
+      expected: 'MenuBar MenuBar--lighter is-blinking is-visible',
+    },
+    {
+      args: ['Selector', ['heading', 'text'], null, { hoverable: true, highlighted: false }],
+      expected: 'Selector-heading-text is-hoverable',
+    },
+    {
+      args: ['Car', ['roof'], ['shiny', 'large'], { opened: true }],
+      expected: 'Car-roof Car-roof--shiny Car-roof--large is-opened',
+    },
+    {
+      args: ['Selector', null, null, null],
+      expected: 'Selector',
+    },
+  ]
+
+  tests.forEach(({args, expected}) =>
+      t.equal(apply(className, args), expected, 'returns ' + expected)
+  )
+
+  t.end()
+})
