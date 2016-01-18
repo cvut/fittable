78e3645c8ae789b89e862bbc07dbbf10142097d3
Marián Hlaváč <hlavac95@gmail.com>
2016-01-18 12:42:10 +0100

    Fix wrong naming convention implementation

--- src/utils/className.js
+++ src/utils/className.js
@@ -8,18 +8,18 @@ function getKeysOfTruthyValues (object) {
  * Function generating class name according to SUIT CSS naming convention.
  *
  * @param [string] componentName Component name
- * @param [arrayOf(string)] descendants Array of descendants in hierarchic order (-desc1-desc2-...) (optional)
+ * @param [string] descendent Descendent element (optional)
  * @param [arrayOf(string)] modifiers Element modifier (optional)
- * @param [object] states States of component, { selected: true } -> '...-isSelected'
+ * @param [object] states States of component, { selected: true } -> '.is-selected'
  */
-function className (componentName, descendants = null, modifiers = [], states = {}) {
-  const elementName = R.join('-', R.prepend(componentName, descendants))
+function className (componentName, descendent = null, modifiers = [], states = {}) {
+  const elementName = componentName + (descendent ? '-' + descendent : '')
 
   // Construct a strings of modifiers and states
   const modifiersClasses = R.map(modifier => elementName + '--' + modifier, modifiers)
   const stateClasses = R.map(state => 'is-' + state, getKeysOfTruthyValues(states))
 
-  // Join the element name with descendants and modifiers with states
+  // Join the element name with descendents and modifiers with states
   return R.join(' ', R.prepend(elementName, R.concat(modifiersClasses, stateClasses)))
 }
 
--- test/utils/className.test.js
+++ test/utils/className.test.js
@@ -5,31 +5,31 @@ import className from '../../src/utils/className'
 test('className()', t => {
   const tests = [
     {
-      args: ['ComponentName', ['descendants'], ['modifier'], { states: true }],
-      expected: 'ComponentName-descendants ComponentName-descendants--modifier is-states',
+      args: ['ComponentName', 'descendent', ['modifier'], { states: true }],
+      expected: 'ComponentName-descendent ComponentName-descendent--modifier is-states',
     },
     {
-      args: ['AwesomeComponent', ['heading'], [], { blinking: false }],
+      args: ['AwesomeComponent', 'heading', [], { blinking: false }],
       expected: 'AwesomeComponent-heading',
     },
     {
-      args: ['AwesomeComponent', ['footer'], ['darker'], { blinking: true }],
+      args: ['AwesomeComponent', 'footer', ['darker'], { blinking: true }],
       expected: 'AwesomeComponent-footer AwesomeComponent-footer--darker is-blinking',
     },
     {
-      args: ['MenuBar', [], ['lighter'], { blinking: true, visible: true }],
+      args: ['MenuBar', null, ['lighter'], { blinking: true, visible: true }],
       expected: 'MenuBar MenuBar--lighter is-blinking is-visible',
     },
     {
-      args: ['Selector', ['heading', 'text'], [], { hoverable: true, highlighted: false }],
-      expected: 'Selector-heading-text is-hoverable',
+      args: ['Selector', 'text', [], { hoverable: true, highlighted: false }],
+      expected: 'Selector-text is-hoverable',
     },
     {
-      args: ['Car', ['roof'], ['shiny', 'large'], { opened: true }],
+      args: ['Car', 'roof', ['shiny', 'large'], { opened: true }],
       expected: 'Car-roof Car-roof--shiny Car-roof--large is-opened',
     },
     {
-      args: ['Selector', [], [], {}],
+      args: ['Selector', null, [], {}],
       expected: 'Selector',
     },
   ]
