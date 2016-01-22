2c291a65b0955ab196fc224bda5032296fdca8d9
Marián Hlaváč <hlavac95@gmail.com>
2016-01-22 13:13:20 +0100

    Comment the events.js constants

--- src/constants/events.js
+++ src/constants/events.js
@@ -1,3 +1,4 @@
+// Maximum dimensions of Event component in pixels, used for calculating screen overflow.
 export const EVENT_MAX_WIDTH = 300
 export const EVENT_MAX_HEIGHT = 400
 export const EVENT_HEAD_HEIGHT = 80
