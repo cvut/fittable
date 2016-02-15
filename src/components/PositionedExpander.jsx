c2fc1a763b9edfdaa6ec1e6508ad0a5cedcb46c1
Marián Hlaváč <hlavac95@gmail.com>
2016-02-15 13:52:43 +0100

    Add component PositionedExpander

---
+++ src/components/PositionedExpander.jsx
@@ -0,0 +1,66 @@
+import { createClass, PropTypes, cloneElement } from 'react'
+import { findDOMNode } from 'react-dom'
+import safeExpandingDirection from '../utils/safeExpandingDirection'
+import R from 'ramda'
+
+import { EVENT_MAX_WIDTH, EVENT_MAX_HEIGHT } from '../constants/events'
+
+function getExpandingDirection (ref, defaultDir) {
+  const expandableDOM = findDOMNode(ref)
+
+  const point = expandableDOM
+      ? R.props(['left', 'top'], expandableDOM.getBoundingClientRect())
+      : [0, 0]
+
+  return safeExpandingDirection(
+    point,
+    [EVENT_MAX_WIDTH, EVENT_MAX_HEIGHT],
+    global,
+    defaultDir)
+}
+
+const PositionedExpander = createClass({
+
+  propTypes: {
+    expanded: PropTypes.bool,
+    left: PropTypes.number,
+    top: PropTypes.number,
+    children: PropTypes.element.isRequired,
+  },
+
+  displayName: 'PositionedExpander',
+
+  defaultExpanding: { horizontal: 1, vertical: 1 },
+
+  getDefaultProps: () => ({
+    expanded: false,
+  }),
+
+  getInitialState () {
+    return { expandingDirection: this.defaultExpanding }
+  },
+
+  componentWillReceiveProps ({ expanded }) {
+    let expandingDirection = this.defaultExpanding
+    if (expanded && this.refs && this.refs.expandable) {
+      expandingDirection = getExpandingDirection(this.refs.expandable, this.defaultExpanding)
+    }
+
+    this.setState({ expandingDirection })
+  },
+
+  render () {
+    const expandingDirection = this.state.expandingDirection
+
+    const childrenProps = {
+      expanded: this.props.expanded,
+      horizontalAlign: expandingDirection.horizontal,
+      verticalAlign: expandingDirection.vertical,
+      ref: 'expandable',
+    }
+
+    return cloneElement(this.props.children, childrenProps)
+  },
+})
+
+export default PositionedExpander
