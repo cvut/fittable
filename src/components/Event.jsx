dd8ea0b811617f943de044c63d0d45251e31d936
Marián Hlaváč <hlavac95@gmail.com>
2016-02-23 16:56:41 +0100

    Expand props in Event component

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -68,8 +68,9 @@ class EventBox extends React.Component {
 
   style (props) {
     const positionStyles = getPositionStyle(props)
+    const { expanded, screenSize } = props
 
-    if (props.expanded && !isScreenSmall(props.screenSize)) {
+    if (expanded && !isScreenSmall(screenSize)) {
       return {
         ...positionStyles,
         maxWidth: EVENT_MAX_WIDTH,
@@ -80,19 +81,21 @@ class EventBox extends React.Component {
   }
 
   classNames (props) {
-    const cls = ['event', props.data._appear]
-    if (props.expanded) {
+    const { data, expanded, colored } = props
+
+    const cls = ['event', data._appear]
+    if (expanded) {
       cls.push('is-opened')
     }
-    if (props.data.cancelled) {
+    if (data.cancelled) {
       cls.push('event--cancelled')
     }
-    if (props.data.replacement) {
+    if (data.replacement) {
       cls.push('event--replacement')
     }
 
-    if (props.colored) {
-      cls.push(`event--${props.data.type}`)
+    if (colored) {
+      cls.push(`event--${data.type}`)
     }
 
     return cls.join(' ')
@@ -129,8 +132,10 @@ class EventBox extends React.Component {
   }
 
   render () {
-    const onClickVal = this.props.expanded ? null : this.props.data.id
-    const headSpaceStyle = this.props.expanded ? {height: EVENT_HEAD_HEIGHT} : {}
+    const { expanded, data, colored, onClick } = this.props
+
+    const onClickVal = expanded ? null : data.id
+    const headSpaceStyle = expanded ? {height: EVENT_HEAD_HEIGHT} : {}
 
     return (
       <div
@@ -140,10 +145,10 @@ class EventBox extends React.Component {
         <div className="inner">
           <div
             className="head-space"
-            onClick={this.props.onClick.bind(null, onClickVal)}
+            onClick={onClick.bind(null, onClickVal)}
             style={headSpaceStyle}>
             <div className="head-name">
-              {this.props.data.course}
+              {data.course}
             </div>
             <div className="head-time">
               {this.displayTime(this.props)}
@@ -152,8 +157,8 @@ class EventBox extends React.Component {
               {this.displayRoom(this.props)}
             </div>
             <div className="head-type">
-              <span className={`short ${this.props.colored ? ' hide' : ''}`}>
-                {CP.translate('event_type_short.' + this.props.data.type)}
+              <span className={`short ${colored ? ' hide' : ''}`}>
+                {CP.translate('event_type_short.' + data.type)}
               </span>
             </div>
           </div>
