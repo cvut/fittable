1e47a5977bf240df1e3d17bcfc6b3a54c81adfc9
Marián Hlaváč <hlavac95@gmail.com>
2016-03-08 16:17:52 +0100

    Rename configuration variable TZ_OFFSET

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const TZ = 'Europe/Prague'
-export const TZOffset = -1
+export const TZ_OFFSET = -1
 
 export const SIRIUS_PROXY_PATH = global.SIRIUS_PROXY_PATH || process.env.SIRIUS_PROXY_PATH
 
